2b2363be4f6c077edfd4ec66f257f681a6cce00f
teashirtt <1223696764@qq.com>
2024-04-22 22:05:08 +0800

    feat: finish 5140,5153

---
+++ questions/5140-medium-trunc/template.ts
@@ -0,0 +1,1 @@
+type Trunc<T extends string | number> = `${T}` extends `${infer L}.${infer _}` ? L extends '' ? '0' : L : `${T}`
---
+++ questions/5153-medium-indexof/template.ts
@@ -0,0 +1,4 @@
+
+type IndexOf<T extends any[], U, P extends any[] = []> = T['length'] extends 0 ? -1 : T extends [infer L, ...infer _] ? Equal<L, U> extends true ? P['length'] : IndexOf<Shift<T>, U, [...P, 0]> : never
+
+type c = IndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>
